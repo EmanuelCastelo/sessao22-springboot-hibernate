ed7255db160e49a65be7655afea70531b6934019
EmanuelCastelo <jescbf@yahoo.com.br>
2024-12-30 20:17:13 -0300

    Exception handling - update

--- src/main/java/com/ec/sessao22_springboot_hibernate/services/UserService.java
+++ src/main/java/com/ec/sessao22_springboot_hibernate/services/UserService.java
@@ -12,6 +12,8 @@ import com.ec.sessao22_springboot_hibernate.repositories.UserRepository;
 import com.ec.sessao22_springboot_hibernate.services.exceptions.DatabaseException;
 import com.ec.sessao22_springboot_hibernate.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -41,9 +43,14 @@ public class UserService {
 	}
 
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);	
+		} catch (EntityNotFoundException e) {			
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
